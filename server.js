00c54b76fe6574b6283b4f91a5bda3d36de74aff
Anubhav Saxena <saxenaanubhav1204@gmail.com>
2024-04-24 16:31:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from "express";
 const app=express();
 app.get("/",(req,res)=>{
-    res.send("<h1>Hello,World<h1>");
+    res.send("<h2>Hello,World<h2>");
+});
+app.get("/name",(req,res)=>{
+    res.send("<h2>Anubhav<h2>");
 }
 );
 app.listen(8000,() => {

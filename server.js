2288851e36aae8af351258ac910de8ae9babf8bb
Anubhav Saxena <saxenaanubhav1204@gmail.com>
2024-04-24 16:23:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import express from "express";
 const app=express();
+app.get("/",(req,res)=>{
+    res.send("<h1>Hello,World<h1>");
+}
+);
 app.listen(8000,() => {
     console.log("Server listening on port 8000");
 });
